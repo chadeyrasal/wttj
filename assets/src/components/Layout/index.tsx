24334e0d6070070e5e7faf889f4960f93508f6b7
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2025-01-28 17:39:20 +0000

    add logout button

--- assets/src/components/Layout/index.tsx
+++ assets/src/components/Layout/index.tsx
@@ -2,8 +2,11 @@ import { Box } from '@welcome-ui/box'
 import { Flex } from '@welcome-ui/flex'
 import { Link } from '@welcome-ui/link'
 import { Link as RouterLink, Outlet } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
 
 function Layout() {
+  const { isAuthenticated, logout } = useAuth()
+
   return (
     <Box backgroundColor="beige-20" h="100vh">
       <Box backgroundColor="black" p={20}>
@@ -11,8 +14,12 @@ function Layout() {
           <Link as={RouterLink} to="/" color="white">
             <div>Jobs</div>
           </Link>
+          <Box display="flex" justifyContent="flex-end" p="md">
+            {isAuthenticated && <button onClick={logout}>Logout</button>}
+          </Box>
         </Flex>
       </Box>
+
       <Box>
         <Outlet />
       </Box>
